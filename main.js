07338de23d4b19e6511a4ebb37e3d4b0b0a4d41c
Wesley Vance <wslyvnce@gmail.com>
2022-10-11 23:01:45 -0500

    added additional styling and flex features, template literals to js loop

--- main.js
+++ main.js
@@ -254,19 +254,15 @@ const pets = [
 
   for (let i=0; i< pets.length; i++) {
     card.innerHTML += `<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="${pets[i].imageUrl}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${pets[i].name}</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <p class="card-text">${pets[i].specialSkill}</p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">An item</li>
-      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">Species: ${pets[i].type}</li>
+      <li class="list-group-item">Color: ${pets[i].color}</li>
      
     </ul>
-    <div class="card-body">
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
-    </div>
   </div>`
   }
